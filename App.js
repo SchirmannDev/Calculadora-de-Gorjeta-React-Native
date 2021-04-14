888e062d677bd2ef0d6b32b5dcffedd749764a26
schirmann <cschirmann.dev@gmail.com>
2021-04-14 16:35:34 -0300

    style: input

--- App.js
+++ App.js
@@ -4,14 +4,28 @@ import styled from "styled-components/native";
 
 const Container = styled.SafeAreaView`
   flex: 1;
-  justify-content: center;
   align-items: center;
+  margin-top: 30px;
+`;
+
+const HeaderText = styled.Text`
+  font-size: 25px;
+`;
+
+const Input = styled.TextInput`
+  font-size: 18px;
+  width: 90%;
+  height: 40px;
+  background-color: #eee;
+  margin-top: 20px;
+  border-radius: 10px;
 `;
 
 export default () => {
   return (
     <Container>
-      <Text>Olá Marilene</Text>
+      <HeaderText>Calculador de Gorjeta</HeaderText>
+      <Input />
     </Container>
   );
 };
